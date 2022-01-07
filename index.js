523712704dbe12df9cb22ce1f6e31df715a9609c
Phillip Lam <phillip.lam0@gmail.com>
2022-01-06 22:56:57 -0500

    Change playlist source

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ var authOptions = {
 // Populate collab playlist data
 const PLAYLIST_ID_COLLAB = '2akbzrAFt9L9BCjhaQGyUc';
 const PLAYLIST_ID_TEST = '53kbHaKSd8NMsutfybeRSz';
-const PLAYLIST = PLAYLIST_ID_TEST;
+const PLAYLIST = PLAYLIST_ID_COLLAB;
 
 var app = express();
 
