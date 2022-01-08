43f82db8ffb9d55d89aec217007482c18b237056
Phillip Lam <phillip.lam0@gmail.com>
2022-01-08 01:08:04 -0500

    Update: changed root route to redirect to route that updates database at the start

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ var express = require('express'); // Express web server framework
 var cors = require('cors');
 var cookieParser = require('cookie-parser');
 const res = require("express/lib/response");
+const { redirect } = require("express/lib/response");
 var client_id = process.env.SPOTIFY_CLIENT_ID; // Your client id
 var client_secret = process.env.SPOTIFY_CLIENT_SECRET; // Your secret
 
@@ -98,10 +99,12 @@ const textEventHandler = async (event) => {
 // This route is used to receive connection tests.
 app.get('/', async (_, res) => {
 
-    return res.status(200).json({
-        status: 'success',
-        message: 'Connected successfully!',
-    });
+    res.redirect('/manual-update-local-data');
+
+    // return res.status(200).json({
+    //     status: 'success',
+    //     message: 'Connected successfully!',
+    // });
 });
 
 // TODO fix the callback hell below
@@ -327,18 +330,7 @@ app.get('/playlist', async (_, res) => {
 
 app.get('/check-local-data', async (_, res) => {
 
-    let data = fs.readFileSync('total.json');
-    let databaseTotal = JSON.parse(data);
-
-    var JSON_TOTAL = databaseTotal['total'];
-    console.log(JSON_TOTAL);
-
-    res.send(databaseTotal);
-    res.end();
-});
-
-app.get('/manual-update-local-data', async (_, res) => {
-
+    // Get local database value
     let data = fs.readFileSync('total.json');
     let databaseValue = JSON.parse(data);
 
@@ -360,13 +352,47 @@ app.get('/manual-update-local-data', async (_, res) => {
                 if (!error && response.statusCode === 200) {
 
                     // Parse through response
-                    var total = body.tracks.total;
+                    var spotifyTotal = body.tracks.total;
+
+                    res.send({ databaseValue, spotifyTotal });
+                    res.end();
+                }
+            });
+        };
+    });
+});
+
+app.get('/manual-update-local-data', async (_, res) => {
+
+    // Get local database value
+    let data = fs.readFileSync('total.json');
+    let databaseValue = JSON.parse(data);
+
+    request.post(authOptions, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+
+            // use the access token to access the Spotify Web API
+            var token = body.access_token;
+
+            var playlistOptions = {
+                url: 'https://api.spotify.com/v1/playlists/' + PLAYLIST,
+                headers: {
+                    'Authorization': 'Bearer ' + token
+                },
+                json: true
+            };
+
+            request.get(playlistOptions, function (error, response, body) {
+                if (!error && response.statusCode === 200) {
+
+                    // Parse through response
+                    var spotifyTotal = body.tracks.total;
 
                     // Update database value to current value
-                    databaseValue.total = total;
+                    databaseValue.total = spotifyTotal;
                     fs.writeFileSync('total.json', JSON.stringify(databaseValue));
 
-                    res.send(databaseValue);
+                    res.send({ databaseValue, spotifyTotal });
                     res.end();
                 }
             });
